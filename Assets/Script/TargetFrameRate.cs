39d5a7d10c18821507a8d4ea99e4438b4348a9d8
俊杰 张 <chenmoil@outlook.com>
2023-12-10 11:34:35 +0800

    安卓打包尝试

---
+++ Assets/Script/TargetFrameRate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFrameRate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;//…Ë÷√60÷°
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
