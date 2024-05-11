6c85c207e1e9f4d7e2ca3e5061bc6922f113cd8b
helloworld114514 <1423201097@qq.com>
2024-05-11 19:32:59 +0800

    修bug

---
+++ Assets/Script/MadebyZou/RE.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RE : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("decreaseImpetuousLevel", 0);
+        PlayerPrefs.SetInt("shieldLevel", 0);
+        PlayerPrefs.SetInt("moveSpeedLevel", 0);
+        PlayerPrefs.SetInt("fullScreenDamageLevel", 0);
+        PlayerPrefs.SetInt("calmdownSkillLevel", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
